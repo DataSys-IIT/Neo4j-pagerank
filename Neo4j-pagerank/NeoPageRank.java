f498ac308a3af5238ee37099b7bf18764305ead7
gnu3ra <aballmer42@gmail.com>
2016-04-06 20:18:10 -0500

    Initial Commit

---
+++ Neo4j-pagerank/NeoPageRank.java
@@ -0,0 +1,13 @@
+import org.neo4j.server.plugins.ServerPlugin;
+
+public class NeoPageRank extends ServerPlugin {
+
+	@Name('NeoPageRank')
+	@Description('Pagerank for Neo4j REST api')
+	
+	public Iterable<Node> PageRank() {
+		
+		
+	}
+	
+}
