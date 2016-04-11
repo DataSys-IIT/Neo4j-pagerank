375af3af27636fdd970e0647a2de92730f633a20
gnu3ra <aballmer42@gmail.com>
2016-04-11 14:14:49 -0500

    Made package

--- Neo4j-pagerank/src/org/example/NeoPageRank/NeoPageRank.java
+++ Neo4j-pagerank/src/org/example/NeoPageRank/NeoPageRank.java
@@ -1,3 +1,5 @@
+package org.example.NeoPageRank;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
--- Neo4j-pagerank/src/org/example/NeoPageRank/RankLabel.java
+++ Neo4j-pagerank/src/org/example/NeoPageRank/RankLabel.java
@@ -1,3 +1,4 @@
+package org.example.NeoPageRank;
 import org.neo4j.graphdb.Label;
 
 
