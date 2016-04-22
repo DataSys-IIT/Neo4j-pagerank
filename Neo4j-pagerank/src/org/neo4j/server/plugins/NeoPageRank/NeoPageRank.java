99f41b38301de5dd3027be132260b94845eb531c
gnu3ra <aballmer42@gmail.com>
2016-04-22 02:09:41 -0500

    Now loads in neo4j!

--- Neo4j-pagerank/src/org/neo4j/server/plugins/NeoPageRank/NeoPageRank.java
+++ Neo4j-pagerank/src/org/neo4j/server/plugins/NeoPageRank/NeoPageRank.java
@@ -1,4 +1,4 @@
-package org.example.NeoPageRank;
+package org.neo4j.server.plugins.NeoPageRank;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- Neo4j-pagerank/src/org/neo4j/server/plugins/NeoPageRank/RankLabel.java
+++ Neo4j-pagerank/src/org/neo4j/server/plugins/NeoPageRank/RankLabel.java
@@ -1,4 +1,4 @@
-package org.example.NeoPageRank;
+package org.neo4j.server.plugins.NeoPageRank;
 import org.neo4j.graphdb.Label;
 
 
